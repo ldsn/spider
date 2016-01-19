32006a984ec6cc8eabbc482b6eac6b4566bdda1a
wangzhen <237075806@qq.com>
2016-01-19 16:47:19 +0800

    new file:   func.php 	deleted:    curlUA.php

---
+++ func.php
@@ -0,0 +1,13 @@
+<?php
+function getpage($url = "",$config = Array()){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL,$url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    if(isset($config['ua'])){
+    	curl_setopt($ch, CURLOPT_USERAGENT,$config['ua']);
+    }
+    $rs = curl_exec($ch);
+    curl_close($ch); 
+    return $rs;
+}
+?>
